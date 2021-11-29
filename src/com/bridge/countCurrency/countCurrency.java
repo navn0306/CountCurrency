225f03203f8df438486dcdbfd206a6473339bf36
Navneet <navn0306@gmail.com>
2021-11-29 18:02:19 +0530

    ADD: Added the decription for the Project

---
+++ src/com/bridge/countCurrency/countCurrency.java
@@ -0,0 +1,7 @@
+package com.bridge.countCurrency;
+
+public class countCurrency {
+    public static void main(String[] args) {
+        System.out.println("\nWelcome, In this program There is 1, 2, 5, 10, 50, 100, 500 and 1000 Rs Notes which can be returned by Vending Machine. It read the Change in Rs to be returned by the Vending Machine and check for largest value of the Note to return change to get to the minimum number of Notes.");
+    }
+}
