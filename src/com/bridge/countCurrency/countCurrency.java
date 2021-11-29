b447682204ff9c122d4328368e518710a320f349
Navneet <navn0306@gmail.com>
2021-11-29 18:56:08 +0530

    ADD: Added feature to count the number to note required to give the change by vending machine.

--- src/com/bridge/countCurrency/countCurrency.java
+++ src/com/bridge/countCurrency/countCurrency.java
@@ -1,7 +1,38 @@
 package com.bridge.countCurrency;
 
+import java.util.*;
+import java.lang.*;
+
 public class countCurrency {
-    public static void main(String[] args) {
-        System.out.println("\nWelcome, In this program There is 1, 2, 5, 10, 50, 100, 500 and 1000 Rs Notes which can be returned by Vending Machine. It read the Change in Rs to be returned by the Vending Machine and check for largest value of the Note to return change to get to the minimum number of Notes.");
+
+    public static void Currency(int amount) {
+        int[] notes = new int[]{2000, 500, 200, 100, 50, 20, 10, 5, 1};
+        int[] noteCounter = new int[9];
+
+        for (int i = 0; i < 9; i++)
+            if (amount >= notes[i]) {
+                noteCounter[i] = amount / notes[i];
+                amount = amount - noteCounter[i] * notes[i];
+            }
+        System.out.println("Currency Count ->");
+            int noteCount = 0;
+        for (int i = 0; i < 9; i++) {
+            if (noteCounter[i] != 0) {
+                System.out.println(notes[i] + " : " + noteCounter[i]);
+                noteCount += noteCounter[i];
+
+            }
+        }
+        System.out.println("Total notes -> " +noteCount);
+    }
+
+    public static void main(String argc[]) {
+
+        Scanner input = new Scanner(System.in);
+        int amount;
+        System.out.println("Enter the amount");
+        amount = input.nextInt();
+        Currency(amount);
+
     }
 }
